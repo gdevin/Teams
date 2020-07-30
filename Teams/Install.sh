8d13d968dc61f1b021609a2acc34e72247230ae4
Devin G <gdevin@hotmail.com>
2020-07-30 08:54:31 -0500

    Translated Install.sh file to english and edited it to work on Ubuntu-Mate.

--- Teams/Install.sh
+++ Teams/Install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#Elias Baumann 03/2020
-
 BLINK='\033[5;96m'
 RED='\033[0;31m'
 GREEN='\033[1;92m'
 CYAN='\033[1;96m'
 PURPLE='\033[0;95m'
-NC='\033[0m'  #NC steht hier für NoColour
+NC='\033[0m'  #NC stands for NoColour here
 
-progress-bar() {  #Ein Ladebalken der als Parameter Zeit in Sekunden annimmt.
+progress-bar() {  #A loading bar that takes time in seconds as a parameter.
   local duration=${1}
 
 
@@ -26,16 +24,16 @@ progress-bar() {  #Ein Ladebalken der als Parameter Zeit in Sekunden annimmt.
   clean_line
 }
 
-echo -e "${BLINK}Die Installation läuft! Bitte warten...${NC}"
-progress-bar 20 #Die Zwanzig ist die Zeit bis Countdown fertig ist.
-sudo cp -f /home/pi/Desktop/Teams/Teams/Teams/msteams.png /usr/share/pixmaps/msteams.png& #Hier wird das PNG von dem Icon nach pixmaps kopiert
-sudo cp -f /home/pi/Desktop/Teams/Teams/Teams/Teams.desktop /home/pi/Desktop/Teams.desktop& 	#Die Desktopdatei wird kopiert
-sudo cp -fR /home/pi/Desktop/Teams/Teams/Teams /home/pi/Documents/Teams& 	 #Der Ordner mit der dem Teams-skript wird kopiert
-sudo chmod a+x /home/pi/Desktop/Teams.desktop&  #Die Desktopdatei wir ausführbar gemacht
-sudo chmod a+x /home/pi/Documents/Teams/Teams.sh& #Das Teams-skript wird ausführbar gemacht
-sleep 0.5 #Delay um vorzeitiges Löschen des Ordners zu verhindern (Ohne dem wird der Ordner vor dem abschließen der vorhergegangenen Befehle gelöscht!)
-sudo rm -r /home/pi/Desktop/Teams&  #Hier wird der auf den Dektop kopierte Ordner gelöscht.
-echo -e "${CYAN}Der Installationsprozess wurde${NC} ${GREEN}abgeschlossen!${NC}"
+echo -e "${BLINK}The installation is running! Please wait...${NC}"
+progress-bar 20 #Twenty is the time until the countdown is finished.
+sudo cp -f ~/Desktop/Teams/Teams/Teams/msteams.png /usr/share/pixmaps/msteams.png& #Here the PNG is copied from the icon to pixmaps
+sudo cp -f ~/Desktop/Teams/Teams/Teams/Teams.desktop ~/Desktop/Teams.desktop& 	#The desktop file is copied
+sudo cp -fR ~/Desktop/Teams/Teams/Teams ~/Documents/Teams& 	 #The folder with the team script is copied
+sudo chmod a+x ~/Desktop/Teams.desktop&  #The desktop file is made executable
+sudo chmod a+x ~/Documents/Teams/Teams.sh& #The team script is made executable
+sleep 0.5 #Delay to prevent the folder from being deleted prematurely (without this, the folder will be deleted before the previous commands are completed!)
+sudo rm -r ~/Desktop/Teams&  #The folder copied to the desktop is deleted here.
+echo -e "${CYAN}The installation process was${NC} ${GREEN}completed!${NC}"
 sleep 2
 clear
 exit
